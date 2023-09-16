f0f5e78ebc7ff191135b53e6132a3f29b5aa0d4e
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 <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>
2023-09-16 16:24:32 +0700

    working BUT: exec_read func misses flags at the end of the token_buff

--- IPC/bash_IPC.c
+++ IPC/bash_IPC.c
@@ -11,37 +11,25 @@
 char* readline() {
   char* line;
   ssize_t buff;
+  printf("Enter command: ");
   getline(&line, &buff, stdin);
+  line[strcspn(line, "\n")] - '\0';
   return line;
 }
 
-//  Разбиение строки ввода на команды по разделителю "|"
-char** parcer_command(char* in_line) {
-  int position = 0;
-  char* token;
-  char** out_tokens = malloc(sizeof(char) * 256);
-  const char* separator = "|";
-  token = strtok(in_line, separator);
-  while (token != NULL) {
-    out_tokens[position++] = token;
-    token = strtok(NULL, separator);
-  }
-  out_tokens[position] = NULL;
-  return out_tokens;
-}
+
 
 //  Разбиение строки ввода на токены
-char** parcer(char* in_line) {
+char** parcer(char* in_line, const char* separator) {
   int position = 0;
   char* token;
-  char** out_tokens = malloc(sizeof(char) * 256);
-  const char* separator = " \n";
+  char** out_tokens = malloc(sizeof(char) * 1024);
   token = strtok(in_line, separator);
   while (token != NULL) {
     out_tokens[position++] = token;
     token = strtok(NULL, separator);
   }
-  out_tokens[position] = NULL;
+  //out_tokens[position] = NULL;
 
   //------------------------------------------
   //  ДЛЯ ПРОВЕРКИ УДАЛИТЬ ПО ЗАВЕРШЕНИЮ
@@ -58,77 +46,75 @@ char** parcer(char* in_line) {
 //  Проблема: вроде функции работают по отдельности 
 //  но вывод первой не передается на вход второй
 //  Видимо проблемы с передачей дескриптора
-void pipe_work(int pipefd[],char** token_list){
-  if (pipe(pipefd) == -1) {
-    err_exit("pipe");
-  }
-
-  if (write(pipefd[1], stdout, 100*sizeof(char)) == -1)
-  {
-    err_exit("dup2_1");
-  }
-  close(pipefd[1]);
-  if (dup2(pipefd[0], 0) == -1)
-  {
-    err_exit("dup2_0");
-  }
-  close(pipefd[0]);
-
+//void pipe_work(int pipefd[],char** token_list){
   
-  if (execvp(token_list[0], token_list) == -1)
-  {
-    err_exit("pipe_work --> execvp");
-  }
   
-}
+//}
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+//  Выполнение команды с записью в дескриптор
+void exec_write(int filedes[], char** cmd){
+    if (filedes[1] != STDOUT_FILENO)
+    {
+        if (dup2(filedes[1], STDOUT_FILENO) == -1) err_exit("exec_write:  dup2");
+        if (close(filedes[1]) == -1) err_exit("exec_write:  close 1");
+    }
+    if(execvp(cmd[0], cmd) == -1) err_exit("write exec");
+}
+
+//  Выполнение команды с прочитыванием из дескриптора
+void exec_read(int filedes[], char** cmd){
+    if (filedes[0] != STDIN_FILENO)
+    {
+        if (dup2(filedes[0], STDIN_FILENO) == -1) err_exit("dup2");
+        if (close(filedes[0]) == -1) err_exit("close 2");
+    }
+    
+    if(execvp(cmd[0], cmd) == -1) err_exit("read exec");
+}
+
 
 //  Основная работа
-void processing(char** tokens_list) {
-  pid_t child_pid;
-  int status;
+void processing(char* line) {
   int pipefd[2];
-  child_pid = fork();
-  if (child_pid == 0) {
-    pipe_work(pipefd,tokens_list);
-  } else {
-    waitpid(child_pid, &status, 0);
-  }
-}
-
-//  Основная работа
-void processing_old(char** tokens_list) {
-  pid_t child_pid;
-  int status;
-  child_pid = fork();
-  if (child_pid == 0) {
-    execvp(tokens_list[0], tokens_list);
-  } else {
-    waitpid(child_pid, &status, 0);
-  }
-}
-
-//  По идее должна принимать строку из parcer_command 
-//  и вызывать parcer для каждой команды через pipe
-void making_pipes(char** token_list) {
-  char* buffer[sizeof(token_list)];
-  for (int i = 0; i < sizeof(token_list); i++)
+  if (pipe(pipefd) == -1) err_exit("pipe");
+  char **command_buff;                  //  Буффер разеделния по "|"
+  char **token_buff;                    //  Буффер разделения по пробелам
+  command_buff = parcer(line, "|");     //  Делим ввод на комнады через "|"
+  for (size_t i = 0; i < sizeof(command_buff); i++)
   {
-    buffer[i] =  token_list[i];            //  Передача в парсер токенов отдельных комманд
+    printf("out -->  %s\n", command_buff[i]);
   }
-  for (size_t i = 0; i < sizeof(token_list); i++)
-  {
-    processing(parcer(buffer[i]));         //  Передача в парсер токенов отдельных комманд
-  }
+  switch(fork()){
+    case -1:
+        err_exit("fork");
+        break;
+    case 0:                             /*  Дочерний exec: "ls" с записью в дескриптор */
+        close(pipefd[0]);
+        token_buff = parcer(command_buff[0], " ");  //  Делим ввод на комнады через пробелы
+        for (size_t i = 0; i < sizeof(token_buff); i++)
+        {
+          printf("token_buff_write -->  %s\n", token_buff[i]);
+        }
+        exec_write(pipefd, token_buff);
+        break;
+    default:                            /*  Подительский exec: "grep" с считыванием из дескриптор */
+        close(pipefd[1]);
+        token_buff = parcer(command_buff[1], " ");
+        for (size_t i = 0; i < sizeof(token_buff); i++)
+        {
+          printf("token_buff_read -->  %s\n", token_buff[i]);
+        }
+        exec_read(pipefd, token_buff);
+        break;
+    }
 }
-
-
-
-
 
 int main() {
-  //processing(parcer(parcer_command(readline())));
-  making_pipes(parcer_command(readline()));
+  
+  char * buff = readline();
+  char ** token_buff;
+  processing(buff);
+
   return 0;
 }
